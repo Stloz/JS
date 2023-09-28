c682f84e37827c5bb7bd3f283d60ab94f85a4501
Stloz <stloz.serg@gmail.com>
2023-09-28 18:01:45 +0300

    import - export

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,12 @@
+import {
+  message as Message1,
+  message2 as Message2,
+} from '../../script/test'
+
+import MyName from '../../script/test'
+
 import '../../script/test'
 
 console.log('container')
+console.log(Message1, Message2)
+console.log(MyName)
--- src/script/test.js
+++ src/script/test.js
@@ -1,1 +1,13 @@
-console.log('script')
+// console.log('script')
+
+export const message = 'Default text'
+
+const message = () => {}
+
+function Message3() {}
+
+const message3 = 'script'
+
+export { message3, Message3, message2 }
+
+export default message
